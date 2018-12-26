90d450a0609283c3843554d77a1f3aa9e3b0b180
BrianRhodes2018 <42330802+BrianRhodes2018@users.noreply.github.com>
2018-12-26 13:00:04 -0500

    Update app.js

--- app.js
+++ app.js
@@ -14,3 +14,38 @@ var tbody = d3.select("tbody");
    });
  });
 
+var submit = d3.select("#filter-btn");
+
+submit.on("click", function() {   
+     
+     
+    d3.event.preventDefault();
+     
+     
+     var inputElement = d3.select("#datetime");
+     
+     
+     var inputValue = inputElement.property("value");
+     
+     console.log(inputValue);
+     console.log(tableData);
+   
+   
+    
+   var filteredData = tableData.filter(date => date.datetime === inputValue);
+    
+   console.log(filteredData);
+    
+   var tbody = d3.select("tbody");
+    
+    filteredData.forEach(function(fData) {
+          console.log(fData);
+          var row = tbody.append("tr");
+          Object.entries(fData).forEach(function([key, value]) {
+          console.log(key, value);
+           var cell = row.append("td");
+           cell.text(value);
+         });
+    });
+   });
+
